23fad8c98aabd8683ac0636dfb59430ab589c2f8
Gerry Gleason <gerrygleason5700@gmail.com>
2018-09-28 10:08:45 -0500

    Initial version of exporter, external file handling incomplete, works for seed data

--- go/cmd/import/import.go
+++ go/cmd/import/import.go
@@ -2,10 +2,13 @@
 package main
 
 import (
-	"strconv"
-	"math/big"
 	"fmt"
 	"time"
+	"os"
+	"io/ioutil"
+	"encoding/json"
+	"path"
+	"path/filepath"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -34,7 +37,7 @@ type Card struct {
 	Trash             bool
 	Type              *Card
 	TypeID            uint
-	Content string // text (large)
+	Content string   `gorm:"column:db_content" sql:"type:text"` // text (large)
 	nodeRef string
 }
 
@@ -45,10 +48,14 @@ var children [](map[string]uint)
 func id2card(id uint) *Card { return &cards[byid[id]] }
 func id2children(id uint) map[string]uint { return children[byid[id]]}
 
+var dbuser string = "root"
+var dbpw string = "decko123"
+var dbname string = "mydev_development"
+var outdir string = "/home/gerry/expdeck/"
+var srcdir string = "/home/gerry/mysite/files"
+var machineID uint = 18 // could find the type, but this is the only type in seeds with external files
+
 func main() {
-	dbuser := "root"
-	dbpw := "decko123"
-	dbname := "mydev_development"
 	cstring := dbuser+":"+dbpw+"@/"+dbname+"?charset=utf8&parseTime=True&loc=Local"
 	db, err := gorm.Open("mysql", cstring)
 	if err != nil {
@@ -63,109 +70,48 @@ func main() {
 	db.LogMode(true)
 
 	db.Find(&cards)
+	fmt.Printf("Card: %#v\n", cards[1])
+	fmt.Printf("Card: %#v\n", cards[2])
         children = make([]map[string]uint, len(cards))
 	errs = db.GetErrors()
 	if len(errs) > 0 {
 		panic(fmt.Sprintf("Error reading from db: %v", errs))
 	}
 
-	// pass 1: create card_id to array index map
-	for i, c := range cards {
-		byid[c.ID] = uint(i)
-	}
-	// pass 2: create children maps
-	//	children[idx] => map[tagKey] = idx
-	for i, c := range cards {
-		//fmt.Printf("Card[%d, %d] %v, %v %s\n", i, c.ID, c.LeftID, c.RightID, c.Name)
-		if c.LeftID != 0 {
-			lc := id2card(c.LeftID)
-			if lc.ID != c.LeftID { panic(fmt.Sprintf("!= %d %d", lc.ID, c.LeftID)) }
-			lidx := byid[lc.ID]
-			lchrn := id2children(lc.ID)
-			if lchrn == nil {
-				lchrn = map[string]uint{}
-				children[lidx] = lchrn
-			}
-			//fmt.Printf("Left: %s %d lenC:%d\n", lc.Name, lc.ID, len(lchrn))
-			lchrn[id2card(c.RightID).Key] = uint(i)
-			//fmt.Printf("Lft %d: %#v\n", c.LeftID, id2card(c.RightID))
-		}
-	}
-
-	// debugging, dump maps
-	//fmt.Printf("\nPhase change: %d, %d\n\n", len(byid), len(children))
-	/*for id, idx := range byid {
-		c1 := id2card(id)
-		c2 := &cards[idx]
-		fmt.Printf(" [%d]%s => [%d]%s   ", id, c1.Name, idx, c2.Name)
-	}*/
-	/*fmt.Printf("\n\n")
-	for idx, m := range children {
-		c2 := &cards[idx]
-		fmt.Printf(" [%d]%s =>   ", idx, c2.Name)
-		for k, v := range m {
-			c2 := &cards[v]
-			fmt.Printf(" %s => [%d]%s   ", k, idx, c2.Name)
-		}
-		fmt.Printf("\n")
-	}*/
-	//fmt.Printf("\n\n")
-	// pass 3: compute the hashes
+	// Create node diretories and card detail objects
 	for _, c := range cards {
-		//fmt.Printf("ccH1:key:%s\n", c.Key)
-		computeHash("", &c)
+		outputNode(&c)
 	}
 }
 
-func computeHash(idt string, c *Card) (thash *big.Int) {
-	idx := byid[c.ID]
-	if c.nodeRef != "" && c.nodeRef != "started" {
-		//fmt.Printf("%ssk[%s]%s\n", idt, c.Key, c.nodeRef)
-		return
-	}
-	c.nodeRef = "started"
-	//fmt.Printf("%scH[%s]i:%d, %s\n", idt, c.nodeRef, idx, c.Name)
-	tval := ""
-	if c.ID == c.TypeID {
-		//fmt.Printf("%scH2:%d (self-type(Cardtype type))\n", idt, c.ID)
-		tval = `"Cardtype"`
-	} else if byid[c.TypeID] != 0 {
-		t := id2card(c.TypeID)
-		if t.nodeRef == "" {
-			//fmt.Printf("%sccH2type: %d\n", idt, t.ID)
-			computeHash(idt+" ", t)
-		}
-		tval = `{"/": "/ipfs/`+t.nodeRef+`"}`
-		//tval = `{"/": "/ipfs/`+"thehashfortype:"+strconv.Itoa(int(t.ID))+`"}`
-	} else { fmt.Printf("Warn type not in byid %d\n", c.TypeID); return nil }
+func outputNode(c *Card) {
+	nodepath := filepath.Join(outdir, c.Key)
+	err := os.MkdirAll(nodepath, os.ModePerm)
+	check(err)
+	f, err := os.Create(filepath.Join(nodepath, "content.json"))
+	check(err)
+	defer f.Close()
 
-	for _, chidx := range id2children(c.ID) {
-		chld := &cards[chidx]
-		//fmt.Printf("%sccH3ch %s (%d) %s\n", idt, k, chld.ID, chld.Key)
-		computeHash(idt+" ", chld)
+	data, err := json.Marshal(c)
+	check(err)
+	n, err := f.Write(data)
+	if n != len(data) {
+		panic("Incomplete write")
 	}
 
-	/*fmt.Printf("%sCh< ", idt)
-	for k, chidx := range id2children(c.ID) {
-		fmt.Printf("%s => [%d]%s ", k, chidx, cards[idx].Name)
+	if c.TypeID == machineID && c.Content != "" { // write the external file to node dir
+		_, base := path.Split(c.Content)
+		fmt.Printf("ext file %#v\n%s, %s\n", c, filepath.Join(srcdir, c.Content), base)
+		dat, err := ioutil.ReadFile(filepath.Join(srcdir, c.Content))
+		check(err)
+		err = ioutil.WriteFile(filepath.Join(nodepath, base), dat, os.ModePerm)
+		check(err)
 	}
-	fmt.Printf(">\n")*/
-	jsonstring := `{"Name": "`+c.Name+`", "Key": "`+c.Key+`", "Type": `+tval+`", `
-	if c.Content != "" { // inline content, big and file content is a content ref
-		jsonstring += `"Content": "`+c.Content+`", `
-	}
-	for k, chidx := range id2children(c.ID) {
-		chld := &cards[chidx]
-		jsonstring += `"`+k+`": {"/": "`+chld.nodeRef+`"}, `
-	}
-	jsonstring += `}`
-	fmt.Printf("\n%sjson[%d] %s\n", idt, c.ID, jsonstring)
-	// compute hash from json
-	myhash := new(big.Int)
-	c.nodeRef = "jhshdata:"+c.Key+":"+strconv.Itoa(int(c.ID)) //myhash.String()
-	fmt.Printf("%sRet %d %s\n", idt, idx, c.Name)
-	//fmt.Printf("Ret %d %s\n", c.ID, c.nodeRef)
-	return myhash
 }
 
-
+func check(e error) {
+    if e != nil {
+        //fmt.Printf("Warn: %#v\n", e)
+        panic(e)
+    }
+}
