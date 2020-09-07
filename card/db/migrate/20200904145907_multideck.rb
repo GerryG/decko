cf8f3fde986344856f7a320bb1b969e14b814951
Gerry Gleason <gerrygleason5700@gmail.com>
2020-09-07 05:09:56 -0500

    Merge back stashed files with diffs

--- card/db/migrate/20200904145907_multideck.rb
+++ card/db/migrate/20200904145907_multideck.rb
@@ -1,6 +1,6 @@
 class Multideck < ActiveRecord::Migration[6.0]
   def change
-    add_column :cards, :deck_id, :integer, default: 1
+#    add_column :cards, :deck_id, :integer, default: 1
 
     remove_index :cards, column: ['key'], name: "cards_key_index"
     add_index :cards, ['key', 'deck_id'], name: "cards_key_index", unique: true
