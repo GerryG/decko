7a4e65f07a09f7cbbb950d594e9bc7569fe0e5af
Gerry Gleason <gerrygleason5700@gmail.com>
2020-09-10 14:15:36 -0500

    remove commented code

--- decko/lib/decko/rails.rb
+++ decko/lib/decko/rails.rb
@@ -3,12 +3,6 @@ DECKO_RAILS_GEM_ROOT = File.expand_path("../../..", __FILE__)
 module Decko
   if defined? ::Rails::Railtie
     class Railtie < ::Rails::Railtie
-#      initializer "decko-rails.load_task_path",
-#                  before: "decko.engine.load_config_initializers" do
-#        Cardio.set_config ::Rails.application.config
-#        Cardio.set_paths ::Rails.application.paths
-#      end
-
       rake_tasks do |_app|
         load("card/tasks/card.rake")
         load("card/tasks/card/migrate.rake")
